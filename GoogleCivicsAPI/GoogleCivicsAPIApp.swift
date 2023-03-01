c58372c46ca8bfe70a29ac36478866c0dfb396d2
Alex Lenkei <104570139+alexl9412@users.noreply.github.com>
2023-03-01 08:15:57 -0500

    Initial Commit

---
+++ GoogleCivicsAPI/GoogleCivicsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleCivicsAPIApp.swift
+//  GoogleCivicsAPI
+//
+//  Created by Alex Lenkei on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct GoogleCivicsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
